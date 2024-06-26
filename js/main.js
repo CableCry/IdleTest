750161fecdf9de8592f628b18dd4f9b2654dcab2
Caleb Terry <136115632+CableCry@users.noreply.github.com>
2024-06-26 16:56:54 -0400

    💡 #0001
    Refactored main.js to use MVC
    - Model contains game logic
    - View contains Ui updates
    - Controller contains interaction and game loop

--- js/main.js
+++ js/main.js
@@ -1,31 +1,64 @@
-// Create gameData object
+// Following MVC Model -> View -> Controller 
 
-var gameData = {
-  action: {
-    click: 1
+
+// Model
+const gameModel = {
+  data: {
+    action: {
+      click: 0.01
+    },
+    money: {
+      total: 0,
+      perSecond: 0
+    }
   },
-  money: {
-    total: 0,
-    perSecond: 0
+  addMoney() {
+    let tempVal = this.data.money.total;
+    this.data.money.total = parseFloat((this.data.money.total + this.data.action.click).toFixed(2));
+    this.moneyPerSecond(tempVal);
+  },
+  moneyPerSecond(tempVal) {
+    if (this.data.money.total - tempVal > 0) {
+      this.data.money.perSecond = parseFloat(((this.data.money.total - tempVal) * 50).toFixed(2));
+    } else {
+      this.data.money.perSecond = 0;
+    }
+  },
+  // Future logic
+};
+
+
+
+// View 
+const gameView = {
+  updateMoneyDisplay() {
+    document.getElementById("moneyTotal").innerHTML = gameModel.data.money.total;
+    document.getElementById("moneyPerSecond").innerHTML = gameModel.data.money.perSecond;
+  },
+  // Future logic
+};
+
+
+
+// Controller
+const gameController = {
+  init() {
+    this.setupEventListeners();
+    this.startGameLoop();
+  },
+  setupEventListeners() { 
+    document.getElementById("clickMe").addEventListener("click", () => {
+      gameModel.addMoney();
+      gameView.updateMoneyDisplay();
+    })
+  },
+  startGameLoop() {
+    setInterval(() => {
+      gameView.updateMoneyDisplay();
+    }, 1000 / 20)
   }
+  // Future logic
 };
 
-const updateSpeed = 20;
-
-
-
-function addMoney() {
-  gameData.money.total += gameData.action.click;
-};
-
-
-function updateText() {
-    document.getElementById("moneyTotal").innerHTML = gameData.money.total; 
-};
-
-function update() {
-  updateText();
-};
-
-
-setInterval(update, 1000 / updateSpeed)
+gameController.init();
+// console.log('Game initialized');
